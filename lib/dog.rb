8862a2cfba6446b5a05458e3767a68ba60ecb170
ALVIN LU <lualvin1234@gmail.com>
2019-04-02 03:26:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
   def name=(name)
-    @name = name 
-  end 
-  def name 
-    @name 
-  end 
-  def bark 
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark
     puts "woof!"
   end
 end
